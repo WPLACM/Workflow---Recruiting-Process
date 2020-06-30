fe19cadf5edffa31f46033c98c4b6678a0081f87
stewes <s.tewes94@gmail.com>
2020-06-30 08:21:01 +0200

    CheckClientEntryDelegate: added query count

--- src/main/java/org/example/delegate/CheckClientEntryDelegate.java
+++ src/main/java/org/example/delegate/CheckClientEntryDelegate.java
@@ -15,26 +15,29 @@ import javax.persistence.Query;
 public class CheckClientEntryDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        // get candidate Data (Name,...) from Application form
+        // TODO get candidate Data (Name,...) from Application form
         // Candidate: first_name, last_name, birth_date, sex
         String first_name, last_name, sex, email;
         first_name = last_name = sex = email = "test";
         java.sql.Date birth_date = new java.sql.Date(1900, 01, 01);
+
         Candidate applying_candidate = new Candidate(first_name, last_name, birth_date, sex, email);
 
         // query candidate db for candidate
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jdbc:h2:./camunda-dbobjectdb");
         EntityManager entitymanager = emf.createEntityManager();
-        Query query = entitymanager.createQuery("Select e from Candidate e where e.first_name = 'test' ");
-        List<String> list=query.getResultList();
+        Long count = (Long) entitymanager.createQuery( "Select e from Candidate e where e.first_name = 'test' " ).getSingleResult();
 
-        //TODO read query results and set variables
-
-        // if candidate exists
-        delegateExecution.setVariable("CandidateAlreadyExists", true);
-        //delegateExecution.setVariable("Candidate_Id", candidate_id);
-
-        // if candidate is new
-        delegateExecution.setVariable("CandidateAlreadyExists", false);
+        if (count == 0){
+            delegateExecution.setVariable("CandidateAlreadyExists", false);
+            // TODO hand over candidate object/ candidate information to next task?
+        }
+        else if(count == 1){
+            delegateExecution.setVariable("CandidateAlreadyExists", true);
+            // TODO hand over candidate id/ candidate object to next task?
+        }
+        else{
+            // TODO throw error: conflict, multiple entries found
+        }
     }
 }
