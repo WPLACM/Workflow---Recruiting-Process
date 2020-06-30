39eb21e8eab7efc9cfd0c1507958632e8b52ad74
stewes <s.tewes94@gmail.com>
2020-06-30 19:30:40 +0200

    select cvs delegate

--- src/main/java/org/example/delegate/SelectCvsDelegate.java
+++ src/main/java/org/example/delegate/SelectCvsDelegate.java
@@ -1,4 +1,12 @@
 package org.example.delegate;
 
-public class SelectCvsDelegate {
-}
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+    public class SelectCVsDelegate implements JavaDelegate {
+        @Override
+        public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        }
+        // TODO
+    }
