bb9b7a5e0710e28e3ec8f9eb7314b60802a11f82
Maximilian <Maximilian.kania@gmx.de>
2020-07-06 22:02:38 +0200

    Tried to access DemoDataGenerator.java in CamundaApplication.java
    Modified radio buttons in CheckoutForm.html

--- src/main/java/org/example/CamundaApplication.java
+++ src/main/java/org/example/CamundaApplication.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.camunda.bpm.application.PostDeploy;
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CamundaApplication {
   public static void main(String... args) {
     SpringApplication.run(CamundaApplication.class, args);
+  }
+
+  //Gotta figure out how this is called. Taken from github invoice example - InvoiceProcessApplication
+  @PostDeploy
+  public void startFirstProcess(ProcessEngine processEngine) {
+    createUsers(processEngine);
+  }
+  private void createUsers(ProcessEngine processEngine) {
+    // create demo users
+    new DemoDataGenerator().createUsers(processEngine);
   }
 }
