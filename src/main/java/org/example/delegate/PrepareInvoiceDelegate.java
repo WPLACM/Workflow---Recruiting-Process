fac555108d8ae44af87610830614ac4c2e5e0a79
Nils Rethschulte <nils.rethschulte@gmx.de>
2020-07-06 11:47:26 +0200

    configured delegate classes for testing, changed the variables to java object instead of typed values (see https://docs.camunda.org/manual/7.5/user-guide/process-engine/variables/)

--- src/main/java/org/example/delegate/PrepareInvoiceDelegate.java
+++ src/main/java/org/example/delegate/PrepareInvoiceDelegate.java
@@ -8,10 +8,12 @@ import org.camunda.spin.plugin.variable.value.JsonValue;
 
 public class PrepareInvoiceDelegate implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        int number_acceptances = Integer.parseInt(delegateExecution.getVariable("number_of_acceptances").toString());
-        int paymentInfo = Integer.parseInt(delegateExecution.getVariable("payment_information_acceptances").toString());
+        Integer number_acceptances = (Integer) delegateExecution.getVariable("number_acceptances");
+        Integer payment_info = (Integer) delegateExecution.getVariable("payment_info");
+        //int number_acceptances = Integer.parseInt(delegateExecution.getVariable("number_of_acceptances").toString());
+        //int paymentInfo = Integer.parseInt(delegateExecution.getVariable("payment_information_acceptances").toString());
 
-        int net=number_acceptances*paymentInfo;
+        int net=number_acceptances*payment_info;
         delegateExecution.setVariable("net", net);
         double tax=net*0.19;
         delegateExecution.setVariable("tax", tax);
