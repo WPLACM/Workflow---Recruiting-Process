9fe076314be71d1d50d3a1533a6361e1f1171d4d
narno03 <niklasarnold235@gmail.com>
2020-07-07 16:54:17 +0200

    billing

--- src/main/java/org/example/delegate/PrepareInvoiceDelegate.java
+++ src/main/java/org/example/delegate/PrepareInvoiceDelegate.java
@@ -3,7 +3,6 @@ package org.example.delegate;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-
 public class PrepareInvoiceDelegate implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) throws Exception {
         Integer number_of_acceptances = (Integer) delegateExecution.getVariable("number_of_acceptances");
@@ -12,15 +11,26 @@ public class PrepareInvoiceDelegate implements JavaDelegate {
         //Manual setting of invoice id
         String invoiceid = (String) delegateExecution.getVariable("openingid");
         invoiceid = invoiceid + date;
+        Integer open = (Integer) delegateExecution.getVariable("open_spots");
 
-        Double net = number_of_acceptances*payment_info;
-        delegateExecution.setVariable("net", net);
-        Double tax = net*0.19;
-        delegateExecution.setVariable("tax", tax);
-        Double gross = net+tax;
-        delegateExecution.setVariable("gross", gross);
+        try{
+            if(number_of_acceptances>open){
+                throw new Exception();
+            }
+            else{
+                Double net = number_of_acceptances*payment_info;
+                delegateExecution.setVariable("net", net);
+                Double tax = net*0.19;
+                delegateExecution.setVariable("tax", tax);
+                Double gross = net+tax;
+                delegateExecution.setVariable("gross", gross);
 
-        delegateExecution.setVariable("invoiceid", invoiceid);
+                delegateExecution.setVariable("invoiceid", invoiceid);
+            }
+        }
+        catch (Exception e){
+            System.out.print("The partner accepted more candidates than were predefined in the job opening.");
+        }
     }
 
 }
