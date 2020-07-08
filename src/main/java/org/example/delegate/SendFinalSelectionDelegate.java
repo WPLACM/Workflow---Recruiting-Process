a31779d6564c15d09b5b9c84868a5d8006171ef7
stewes <s.tewes94@gmail.com>
2020-07-08 20:16:32 +0200

    changed name of wbig_processInstanceId process variable in SendFinalSelectionDelegate

--- src/main/java/org/example/delegate/SendFinalSelectionDelegate.java
+++ src/main/java/org/example/delegate/SendFinalSelectionDelegate.java
@@ -21,7 +21,7 @@ public class SendFinalSelectionDelegate implements JavaDelegate {
                 (ObjectValue) delegateExecution.getVariableTyped("finalSelection");
 
         ApplicationMessageList final_selection = (ApplicationMessageList) typedApplicationCollection.getValue();
-        String wbig_processInstanceId = (String) delegateExecution.getVariable("WBIG_process_ID");
+        String wbig_processInstanceId = (String) delegateExecution.getVariable("wbig_processInstanceId");
 
         RestTemplate template = new RestTemplate();
         try {
--- src/main/java/org/example/utility/wbigRestEndpoints.java
+++ src/main/java/org/example/utility/wbigRestEndpoints.java
@@ -7,7 +7,7 @@ public class wbigRestEndpoints {
     private static String Localhost = "http://localhost:8080/";
     private static String wbigURL = "http://wfm-group-1.uni-muenster.de:80/";
 
-    private static String current_URL = wbigURL;
+    private static String current_URL = Localhost;
 
     public static String getCurrent_URL() {
         return current_URL;
