36ccb377c81a5eb5e7c5c0eca378e94b4b3a7c00
Nils Rethschulte <nils.rethschulte@gmx.de>
2020-07-09 17:33:51 +0200

    Inserted an if case in case the pathVariable isnt filled

--- src/main/java/org/example/controller/ChangesAcceptedController.java
+++ src/main/java/org/example/controller/ChangesAcceptedController.java
@@ -23,6 +23,9 @@ public class ChangesAcceptedController {
     @PostMapping(path = "/ChangesAccepted/{id}" , consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public String continueBillingProcess (@RequestBody JobOpeningInformation jobInfo, @PathVariable ("id") String wplacm_processInstanceId) throws SQLException {
 
+        if (wplacm_processInstanceId == null) {
+            wplacm_processInstanceId = jobInfo.getWBIG_processInstanceID();
+        }
         //correlation specification via message name "CandidatesPlacedMessage". This needs to be inserted as message name for catching event in bpmn-model.
         runtimeService.createMessageCorrelation("ChangesAccepted")
                 //.processInstanceVariableEquals("WBIG_process_ID", wbig_processInstanceId)
