2e7d86e881ef08f1714c40050dbbc7279396944d
Maximilian <Maximilian.kania@gmx.de>
2020-07-10 14:52:45 +0200

    Set openingId dynamically

--- src/main/java/org/example/listener/PublishJobOpeningEndListener.java
+++ src/main/java/org/example/listener/PublishJobOpeningEndListener.java
@@ -3,11 +3,21 @@ package org.example.listener;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
 
+import java.sql.*;
+
 public class PublishJobOpeningEndListener implements ExecutionListener {
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
-        // TODO  set to openingId created in DB
-        Integer test = 1;
-        delegateExecution.setVariable("openingId", test );
+        int id = (Integer) delegateExecution.getVariable("jobProfileId");
+
+        String count_query = "SELECT jobOpeningId FROM Job_Opening " +
+                "WHERE FK_JobProfileId = " + Integer.toString(id);
+
+        Connection con = DriverManager.getConnection("jdbc:h2:./camunda-db", "sa", "sa");
+        Statement query = con.createStatement();
+        ResultSet rs = query.executeQuery( count_query);
+        if(rs.next()) {
+            delegateExecution.setVariable("openingId", rs.getInt("jobOpeningId"));
+        }
     }
 }
